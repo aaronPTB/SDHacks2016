a683d1652fa611ac7c72e571fcc6414100ec3399
aaronptb <aaron.ptb@gmail.com>
2016-10-01 15:09:25 -0500

    update

--- app/app.py
+++ app/app.py
@@ -1,17 +1,22 @@
 from config import *
-from flask import Flask
+from flask import Flask, request, redirect, url_for, send_from_directory
 from flask import request
 
-## Configuring the app
+## Configuring the axpp
 app = Flask(__name__)
+app.debug=True
 
 ## URL Building;
 @app.route('/', methods=['GET','POST'])
 def hello_world():
     if request.method =='GET':
-        return 'hello, world!'
+        print HOME
+        return send_from_directory('static', 'pages/home/index.html')
     else:
         def callback():
             return 'received!'
 
         do_action(callback);
+
+if __name__ == 'main':
+    app.run()
--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
 ## DEFINE APP PARAMETERS HERE ##
+HOME = "static/pages/home"
 
 def do_action(callback):
+    ## code goes here
     callback()
