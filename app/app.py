5da19693c8bcf7231e9b5fe10680aeafbd01623a
aaronptb <aaron.ptb@gmail.com>
2016-10-01 12:33:32 -0500

    updates

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from config.py import *
+
+## Configuring the app
 app = Flask(__name__)
 
-@app.route('/')
+## URL Building;
+@app.route('/', methods=['GET','POST'])
 def hello_world():
     return "hello, world!"
