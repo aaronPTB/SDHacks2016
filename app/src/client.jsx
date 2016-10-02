93236892a1dc3b22abfbd8ec666924b2a3aa0c44
aaronptb <aaron.ptb@gmail.com>
2016-10-02 05:28:47 -0500

    update

--- app/src/client.jsx
+++ app/src/client.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App.jsx'
-import Bar from '../components/Bar.jsx'
+import App from '../components/App.jsx';
+import Bar from '../components/Bar.jsx';
+
+import covert from 'convert.jsx';
 
 ReactDOM.render(
   <div>
--- app/src/convert.jsx
+++ app/src/convert.jsx
@@ -24,27 +24,3 @@ function convert(pdf_path) {
 		return JSON.stringify(output);
 	});
 }
-
-// var getWeeks = function(data) {
-// 	var weeknums = [];
-// 	var indices = [];
-// 	re = /(\d{1,2}) +/g;
-// 	while ((match = re.exec(data)) != null) {
-// 		len = weeknums.length-1
-// 		if (match[1] === '1' & weeknums.length === 0) {
-// 			weeknums.push(parseInt(match[1]));
-// 			indices.push(match.index);
-// 		}
-// 		else if (weeknums.length !== 0) {
-// 			if (parseInt(match[1]) === weeknums[len]+1) {
-// 				weeknums.push(parseInt(match[1]));
-// 				indices.push(match.index);
-// 			}
-// 		}
-// 	}
-// 	for (i=0; i<indices.length-1; i++) {
-// 		console.log(data.substring(indices[i], indices[i+1]));
-// 	}
-// }
-
-export default convert
